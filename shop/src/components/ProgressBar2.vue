0296901ebea5e4cbb45b51f715cc1a931eb1249c
fateme ghezloo <fatemeh.ghezloo@gmail.com>
2018-07-21 02:41:06 +0430

    progress bars updated

--- shop/src/components/ProgressBar2.vue
+++ shop/src/components/ProgressBar2.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="progressBar">
+  <div class="progressBar2">
 
     <br/>
     <br/>
-   <img src="https://preview.ibb.co/bWxLmy/Screen_Shot_1397_04_30_at_02_29_39.png" style="width: 1000px;height: 70px;">
+    <img src="https://preview.ibb.co/dcBPDd/Screen_Shot_1397_04_30_at_02_38_04.png" style="width: 1000px;height: 70px;">
     <br/>
     <br/>
+
 
   </div>
 </template>
 
 <script>
   export default{
-    name: 'progressBar',
+    name: 'progressBar2',
     data () {
       return {
 
@@ -25,7 +26,3 @@
 <style scoped>
 
 </style>
-
-
-
-
