f1213ac88b33b5e050deff28af3b618acda3c9ab
fateme ghezloo <fatemeh.ghezloo@gmail.com>
2018-07-20 19:46:12 +0430

    files modified

---
+++ shop/src/components/Slider.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="slider">
+    <v-app id="inspire">
+      <v-carousel>
+        <v-carousel-item
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+        ></v-carousel-item>
+      </v-carousel>
+    </v-app>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'slider',
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  </style>
+
+
+
+
