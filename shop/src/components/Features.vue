9a8375bc3a515beefed0384b1a11099265d7e7bf
fateme ghezloo <fatemeh.ghezloo@gmail.com>
2018-07-20 19:42:28 +0430

    features component added

---
+++ shop/src/components/Features.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="features flex-container">
+
+
+    <div class="flex-item" style="width:50%">
+
+    </div>
+
+    <div class="flex-item" style="width:50%">
+      <p>ویژگی‌ها</p>
+      <ul>
+        <li v-for="f in features" v-text="f.feature">
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'features',
+    data() {
+      return {
+        features: [
+          {
+            feature: 'ویژگی'
+          },
+          {
+            feature: 'ویژگی'
+          },
+          {
+            feature: 'ویژگی'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .features{
+    direction:rtl;
+    position: relative;
+  }
+
+  .flex-container{
+    margin-right: 100px;
+    display: flex;
+    flex-wrap: nowrap;
+    background-color: White;
+    direction:rtl;
+    position:relative;
+    text-align: right;
+    bottom: 30px;
+  }
+
+  .flex-item{
+  }
+
+</style>
