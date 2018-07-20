615fdd351a18dd769f5a5103e42637cf8009c8a5
fateme ghezloo <fatemeh.ghezloo@gmail.com>
2018-07-20 19:34:32 +0430

    product detail page added

---
+++ shop/src/ProductDetails.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="productDetils">
+    <headerr></headerr>
+    <categoryNav></categoryNav>
+    <productDetails></productDetails>
+    <features></features>
+    <footerr></footerr>
+  </div>
+</template>
+
+
+
+<script>
+  import Headerr from '../components/Headerr'
+  import Footerr from '../components/Footerr'
+  import ProductDetails from '../components/ProductDetails'
+  import Features from "../components/Features";
+  import CategoryNav from "../components/CategoryNav";
+
+
+
+
+
+  export default {
+    name: 'productDetils',
+    components: {
+      Headerr, Footerr,ProductDetails, Features, CategoryNav
+    }
+  }
+</script>
+
+<style>
+  #productDetils {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
