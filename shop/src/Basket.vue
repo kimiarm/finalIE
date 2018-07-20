f3eb0061c95bad572ae9c29b0de6b6f83d01c80d
fateme ghezloo <fatemeh.ghezloo@gmail.com>
2018-07-21 01:32:09 +0430

    basket page added

---
+++ shop/src/Basket.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="basket">
+    <headerr></headerr>
+    <ItemsinBasket></ItemsinBasket>
+    <footerr></footerr>
+  </div>
+</template>
+
+
+
+<script>
+  import Headerr from './components/Headerr'
+  import Footerr from './components/Footerr'
+  import ProgressBar from './components/ProgressBar'
+  import CategoryNav from "./components/CategoryNav"
+  import ItemsinBasket from "./components/ItemsInBasket";
+
+
+
+
+
+
+  export default {
+    name: 'basket',
+    components: {
+      CategoryNav,
+      Headerr, Footerr, ProgressBar, ItemsinBasket
+    }
+  }
+</script>
+
+<style>
+  #basket {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
